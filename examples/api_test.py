c83d34b9280ca98520ae1d489937d15528a49a8f
LastGenius-edu <sultanov@ucu.edu.ua>
2020-03-30 13:27:48 +0300

    function wrapping and documentation

--- examples/api_test.py
+++ examples/api_test.py
@@ -8,15 +8,25 @@ import os
 from goodreads import client, request
 
 
-# setting up the API keys from local file
-goodreads_key = os.environ['GOODREADS_KEY']
-goodreads_secret = os.environ['GOODREADS_SECRET']
+def main():
+    """
+    Main function of the test module
+    """
+    
+    # setting up the API keys from local file
+    goodreads_key = os.environ['GOODREADS_KEY']
+    goodreads_secret = os.environ['GOODREADS_SECRET']
 
-gc = client.GoodreadsClient(goodreads_key, goodreads_secret)
+    # creating a client for book search and information retrieval
+    gc = client.GoodreadsClient(goodreads_key, goodreads_secret)
 
-for i in range(1, 3000):
-    try:
-        book = gc.book(i)
-        print(f"{book.title} - published in {dict(dict(book.work)['original_publication_year'])['#text']}")
-    except (request.GoodreadsRequestException, KeyError):
-        continue
+    for i in range(1, 3000):
+        try:
+            book = gc.book(i)
+            print(f"{book.title} - published in {dict(dict(book.work)['original_publication_year'])['#text']}")
+        except (request.GoodreadsRequestException, KeyError):
+            continue
+
+
+if __name__ == "__main__":
+    main()
