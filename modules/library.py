dab08b462fe2acd0833bd498a7c9f74f7f506148
LastGenius-edu <sultanov@ucu.edu.ua>
2020-05-09 16:11:47 +0300

    Abstractize a few classes away, Create a functional library system with documentation

--- modules/library.py
+++ modules/library.py
@@ -5,57 +5,68 @@ MIT License 2020
 """
 
 
-class Author:
+class Category:
     """
-    Author class
+    Category class
+    Acts as an Author, Year
     """
     def __init__(self, name):
         """
-        Creates an author object to contain author's books
+        Creates a Category object to contain category's books
         :name: str
         """
         self.name = name
         self.books = []
 
-    def add_books(self, book):
+    def add_book(self, book):
         """
         Adds books to the author's instance
         """
         assert isinstance(book, Book), "Instance of Book class should be provided"
         self.books.append(book)
 
+    def __str__(self):
+        return f"Category {self.name} contains these books: {str(self.books)}"
 
-class AuthorList:
-    """
-    AuthorList class - a container for authors
-    """
-    def __init__(self):
-        """
-        Creates the AuthorList instance
-        """
-        self.author_list = []
+    def __repr__(self):
+        return f"<Category {self.name}, size={len(self.books)}>"
 
-    def add_author(self, name):
+
+class CategoryList:
+    """
+    CategoryList class - a container for categories
+    """
+    def __init__(self, name):
+        """
+        Creates the CategoryList instance
+        """
+        self.name = name
+        self.categories = []
+
+    def add_category(self, name):
         """
         Adds the author to the list
         """
-        self.author_list.append(Author(name))
+        self.categories.append(Category(name))
 
     def __getitem__(self, item):
-        assert item in self, "Can only get existing authors"
+        assert item in self, "Can only get existing categories"
 
-        for author in self.author_list:
-            if author.name == item:
-                return author
+        for category in self.categories:
+            if category.name == item:
+                return category
 
     def __contains__(self, item):
         """
-        Checks if the author with the name already exists
+        Checks if the category with the name already exists
         """
-        for author in self.author_list:
-            if author.name == item:
+        for category in self.categories:
+            if category.name == item:
                 return True
         return False
+
+    def __str__(self):
+        return f"CategoryList {self.name}: {str(self.categories)}"
 
 
 class Book:
@@ -63,6 +74,14 @@ class Book:
     Book class
     """
     def __init__(self, title, filename, authors, year):
+        """
+        Initializes a Book instance
+
+        :title: str
+        :filename: str
+        :authors: List[Author]
+        :year: Year
+        """
         self.title = title
         self.filename = filename
         self.authors = authors
@@ -83,18 +102,40 @@ class Book:
 
 class Library:
     def __init__(self):
+        """
+        Initializes Library class instance with empty book shelf and no authors.
+        """
         self.general_book_list = []
-        self.authors_list = AuthorList()
+        self.authors_list = CategoryList("Authors")
+        self.published_years = CategoryList("Years")
 
     def add_book(self, title, filename, authors, year):
-        real_authors = []
+        """
+        Adds a book to the library, converting needed parameters
+        to class instances for easier manipulation and search.
+        """
 
+        # If the Author category doesn't yet exist, create and remember him
+        real_authors = []
         for author_name in authors:
 
             if author_name not in self.authors_list:
-                self.authors_list.add_author(author_name)
+                self.authors_list.add_category(author_name)
 
             real_authors.append(self.authors_list[author_name])
 
-        book = Book(title, filename, real_authors, year)
+        # If the Year category doesn't yet exist, create and remember him
+        if year not in self.published_years:
+            self.published_years.add_category(Category(year))
+        real_year = self.published_years[year]
+
+        # Create a Book instance with pre-processed parameters
+        book = Book(title, filename, real_authors, real_year)
+
+        # Adding the reference to this book to authors and published years categories
+        for real_author in real_authors:
+            real_author.add_book(book)
+        self.published_years[year].add_book(book)
+
+        # Add the book to the general bookshelf
         self.general_book_list.append(book)
